956dcc147057dea9d63c5ed518835dd7264f87a3
akash18baaz <akashsharma@Akashs-MacBook-Air.local>
2022-07-31 20:39:15 +0530

    feat - added emoji picker and file selector

--- src/components/new-post/new-post.js
+++ src/components/new-post/new-post.js
@@ -1,23 +1,87 @@
-import "./new-post.css"
-import CollectionsIcon from '@mui/icons-material/Collections';
-import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-function NewPost () {
-    return(
-        <div className="new__post-container">
-            <div className="new__post-left">
-                <img src=""/>
+import "./new-post.css";
+import CollectionsIcon from "@mui/icons-material/Collections";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import Picker from "emoji-picker-react";
+
+function NewPost() {
+  const { user } = useSelector((store) => store.reduxStore);
+  const [postFile, setPostFile] = useState(null);
+  const [chosenEmoji, setChosenEmoji] = useState("");
+  const [showEmojis, setSHowEmojis] = useState(false);
+
+  const onFileChange = async (e) => {
+    let file = e.target.files[0];
+
+    const getImgUrl = (file) => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+      });
+    };
+    const getPostImg = await getImgUrl(file);
+    setPostFile(getPostImg);
+  };
+
+  const onEmojiClick = (event, emojiObject) => {
+    setChosenEmoji((preEmoji) => preEmoji + emojiObject.emoji);
+    setSHowEmojis(false);
+  };
+
+  return (
+    <div className="new__post-container">
+      <div className="new__post-left">
+        <img src={user.profilePic} />
+      </div>
+      <div className="new__post-right">
+        <textarea
+          id="post__text"
+          className="post__text"
+          placeholder="What's happening?"
+          value={chosenEmoji}
+          onChange={(e) => setChosenEmoji(e.target.value)}
+        ></textarea>
+        {postFile ? (
+          <>
+            <div className="file__container">
+              <img src={postFile} />
+              <div className="cancel__btn" onClick={() => setPostFile(null)}>
+                <i class="bx bx-x"></i>
+              </div>
             </div>
-            <div className="new__post-right">
-                <textarea className="post__text" placeholder="What's happening?"></textarea>
-                <div className="post__options">
-                    <CollectionsIcon className="post__icons"/>
-                    <InsertEmoticonIcon className="post__icons"/>
-                    <AddCircleIcon className="post__btn"/>
-                </div>
-            </div>
+          </>
+        ) : null}
+        {showEmojis && (
+          <div className="emoji__container">
+            <Picker onEmojiClick={onEmojiClick} />
+          </div>
+        )}
+        <div className="post__options">
+          <div className="image-upload">
+            <label for="file-input" className="post__icon-label">
+              <CollectionsIcon className="post__icons" />
+            </label>
+
+            <input
+              id="file-input"
+              type="file"
+              onChange={onFileChange}
+              accept="image/apng, image/avif, image/gif, image/jpeg, image/png, image/svg+xml, image/jpg,image/webp"
+            />
+          </div>
+          <InsertEmoticonIcon
+            className="post__icons"
+            onClick={(e) => setSHowEmojis(!showEmojis)}
+          />
+          <AddCircleIcon className="post__btn" />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export {NewPost}
+export { NewPost };
