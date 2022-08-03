eaf03929332385f85d84bd3677be1dac4f0db0ac
akash18baaz <akashsharma@Akashs-MacBook-Air.local>
2022-08-03 14:16:35 +0530

    refactor - fixed design issue and updated new post data

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -10,9 +10,10 @@ export const posts = [
   {
     _id: uuid(),
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Hard work beats talent when talent doesn't work hard.",
+    pic : '',
     likes: {
-      likeCount: 0,
+      likeCount: 2,
       likedBy: [],
       dislikedBy: [],
     },
@@ -24,7 +25,7 @@ export const posts = [
       {
         _id: uuid(),
         username : "akash1307",
-        text : "nice",
+        text : "🙌🏻❤",
         votes : {
           upvotedBy : [],
           downvotedBy : [],
@@ -33,7 +34,7 @@ export const posts = [
       {
         _id: uuid(),
         username : "rajB",
-        text : "Really good",
+        text : "nice pic dear",
         votes : {
           upvotedBy : [],
           downvotedBy : [],
@@ -45,8 +46,10 @@ export const posts = [
   {
     _id: uuid(),
     content:
-    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",    likes: {
-      likeCount: 0,
+    "Life me career stress nhi, career stress me thodi bohot life chal rhi hai 😭😭😭",    
+    pic : '',
+    likes: {
+      likeCount: 3,
       likedBy: [],
       dislikedBy: [],
     },
@@ -58,7 +61,7 @@ export const posts = [
       {
         _id: uuid(),
         username : "adarshbalika",
-        text : "nice",
+        text : "🥺🥺🥺",
         votes : {
           upvotedBy : [],
           downvotedBy : [],
@@ -67,7 +70,7 @@ export const posts = [
       {
         _id: uuid(),
         username : "rajB",
-        text : "Really good",
+        text : "Itna bhi sach nhi bolna tha 😭",
         votes : {
           upvotedBy : [],
           downvotedBy : [],
@@ -79,9 +82,10 @@ export const posts = [
   {
     _id: uuid(),
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "I'm tired of chasing money when will money start chasing me. 🥺👉👈",
+    pic : '',
     likes: {
-      likeCount: 0,
+      likeCount: 1,
       likedBy: [],
       dislikedBy: [],
     },
@@ -93,7 +97,7 @@ export const posts = [
       {
         _id: uuid(),
         username : "adarshbalika",
-        text : "nice",
+        text : "damn buddy 😂",
         votes : {
           upvotedBy : [],
           downvotedBy : [],
@@ -102,7 +106,7 @@ export const posts = [
       {
         _id: uuid(),
         username : "akash1307",
-        text : "Really good",
+        text : "dil pe lagi yeh baat 😫",
         votes : {
           upvotedBy : [],
           downvotedBy : [],
@@ -114,7 +118,8 @@ export const posts = [
   {
     _id: uuid(),
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "That awkward moment between birth and death. 🤌🏻",
+    pic : '',
     likes: {
       likeCount: 0,
       likedBy: [],
@@ -128,7 +133,7 @@ export const posts = [
       {
         _id: uuid(),
         username : "adarshbalika",
-        text : "nice",
+        text : "😂😂😂",
         votes : {
           upvotedBy : [],
           downvotedBy : [],
@@ -137,7 +142,7 @@ export const posts = [
       {
         _id: uuid(),
         username : "akash1307",
-        text : "Really good",
+        text : "",
         votes : {
           upvotedBy : [],
           downvotedBy : [],
@@ -149,9 +154,10 @@ export const posts = [
   {
     _id: uuid(),
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Money has lot to do with how you behave and little to do with how smart you are.",
+    pic : '',
     likes: {
-      likeCount: 0,
+      likeCount: 3,
       likedBy: [],
       dislikedBy: [],
     },
