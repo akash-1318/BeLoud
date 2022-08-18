c882504d4586107567b4dad89b7c330c73e58751
akash18baaz <akashsharma@Akashs-MacBook-Air.local>
2022-08-18 13:30:18 +0530

    feat - added signup redirection

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,10 +1,11 @@
 import "./navigation.css";
-import { DarkMode } from "@mui/icons-material";
 import logo from "../../assets/images/logo.png";
 import AOS from 'aos';
+import { useNavigate } from "react-router-dom";
 AOS.init();
 
 function Navigation() {
+  const navigate = useNavigate();
   return (
     <header className="header" data-aos="fade-down">
       <div className="header_left" data-aos="fade-down" data-aos-delay="200">
@@ -12,8 +13,9 @@ function Navigation() {
         <p className="app_name">BeLoud</p>
       </div>
       <div className="header_right">
-        <button className="signup_btn" data-aos="fade-down" data-aos-delay="100">Signup</button>
-        <DarkMode sx={{ fontSize: 35 }} className="theme_icon" data-aos="fade-down" data-aos-delay="300" />
+        <button className="signup_btn" data-aos="fade-down" data-aos-delay="100" onClick={() => {
+          navigate("/signup")
+        }}>Signup</button>
       </div>
     </header>
   );
